fb9097b818c3ac7ae04e7ea4a82680e7f958a953
Alamh26 <alamh2672004@gmail.com>
2025-07-21 22:31:20 +0100

    game

--- my exercice/images/game.js
+++ my exercice/images/game.js
@@ -1,6 +1,8 @@
- const getrandom1 = () =>{
+ const getrandom = () =>{
  var random=Math.random()*6+1;
      random=Math.floor(random);
+     var random2=(Math.random()*6)+1;
+     random2=Math.floor(random2);
 
      if(random===1){
      document.querySelector(".thefirst img").src="dice1.png"}
@@ -12,22 +14,19 @@
      document.querySelector(".thefirst img").src="dice4.png"}
      else if(random===5){
      document.querySelector(".thefirst img").src="dice1.png"}
-     }
+     
 
-     const getrandom2 = () =>{
- var random=Math.random()*6+1;
-     random=Math.floor(random);
-
-     if(random===1){
+     
+     if(random2===1){
      document.querySelector(".thesecond img").src="dice1.png"}
-     else if(random===2){
+     else if(random2===2){
      document.querySelector(".thesecond img").src="dice2.png"}
-     else if(random===3){
+     else if(random2===3){
      document.querySelector(".thesecond img").src="dice3.png"}
-     else if(random===4){
+     else if(random2===4){
      document.querySelector(".thesecond img").src="dice4.png"}
-     else if(random===5){
+     else if(random2===5){
      document.querySelector(".thesecond img").src="dice1.png"}
      }
-     getrandom1();
-     getrandom2();
+     getrandom();
+     
